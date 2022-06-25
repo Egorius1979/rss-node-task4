04c1e79b2b26514bbca62ccc0517eac59055903b
Egorius1979 <Egoriyvelikolepny1979@gmail.com>
2022-06-25 19:19:43 +0200

    fix: add add color processing

--- src/controller/utils/screen.ts
+++ src/controller/utils/screen.ts
@@ -22,8 +22,17 @@ export const getScreen = async (): Promise<string> => {
   }
 
   let img = robot.screen.capture(x, y, size, size).image;
-
   const jimp = new Jimp({ data: img, width: size, height: size });
+
+  let pos: number = 0;
+  jimp.scan(0, 0, size, size, (_, __, idx) => {
+    jimp.bitmap.data[idx + 2] = img.readUInt8(pos++);
+    jimp.bitmap.data[idx + 1] = img.readUInt8(pos++);
+    jimp.bitmap.data[idx + 0] = img.readUInt8(pos++);
+    jimp.bitmap.data[idx + 3] = img.readUInt8(pos++);
+  });
+
   const resultImg = await jimp.getBase64Async(Jimp.MIME_PNG);
+
   return resultImg.split(",")[1];
 };
